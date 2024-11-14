4e254b29ed9451162f771901f2f4ac0ddba0aa8b
Danilo <150417696+Danran22@users.noreply.github.com>
2024-11-14 16:59:35 -0300

    Arrumando algumas coisas

--- Calculadora/Program.cs
+++ Calculadora/Program.cs
@@ -10,45 +10,55 @@ namespace Calculadora
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Digite seu primeiro numero: ");
+             Console.WriteLine("Digite seu primeiro numero: ");
             int num1 = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Digite a expresão que vai ser usada (+, -, *, /, %): ");
+            expre();
             string exp = Console.ReadLine();
 
             Console.WriteLine("Digite seu segundo numero: ");
             int num2 = Convert.ToInt32(Console.ReadLine());
 
-            if (exp == "/" && num2 == 0)
-            {
+                if (exp == "/" && num2 == 0)
+                {
                 Console.WriteLine("Não é possivel dividir por 0");
-            }
+                }
 
-            float result = 0;
+                float result = 0;
 
-            switch (exp) 
-            {
-                case "+":
-                result = num1 + num2;
-                Console.WriteLine($"O Resultado é {result}");
-                break;
-                case "-":
-                result = num1 - num2;
-                Console.WriteLine($"O Resultado é {result}");
-                break;
-                case "*":
-                result = num1 * num2;
-                Console.WriteLine($"O Resultado é {result}");
-                break;
-                case "/":
-                result = (float) num1 / num2;
-                Console.WriteLine($"O Resultado é {result}");
-                break;
-                case "%":
-                result = (num1 * num2) / 100;
-                Console.WriteLine($"O Resultado é {result}");
-                break;
-            }
+                    switch (exp) 
+                    {
+                        case "+":
+                        result = num1 + num2;
+                        Console.WriteLine($"O Resultado é {result}");
+                        break;
+                        case "-":
+                        result = num1 - num2;
+                        Console.WriteLine($"O Resultado é {result}");
+                        break;
+                        case "*":
+                        result = num1 * num2;
+                        Console.WriteLine($"O Resultado é {result}");
+                        break;
+                        case "/":
+                        result = (float) num1 / num2;
+                        Console.WriteLine($"O Resultado é {result}");
+                        break;
+                        case "%":
+                        result = (num1 * num2) / 100;
+                        Console.WriteLine($"O Resultado é {result}");
+                        break;
+                     }
+        }
+        static void expre()
+        {
+            Console.WriteLine
+            (@"Digite a expresão que vai ser usada 
+            adição (+)
+            Subitração (-)
+            Multiplicação (*)
+            Divisão (/)
+            Porcetagem (%)");
         }
     }
 }
