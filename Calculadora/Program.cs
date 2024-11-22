c9c93493d123a0667175506fe3bed675fbf58737
Danilo <150417696+Danran22@users.noreply.github.com>
2024-11-22 15:00:09 -0300

    Terminei

--- Calculadora/Program.cs
+++ Calculadora/Program.cs
@@ -10,45 +10,55 @@ namespace Calculadora
     {
         static void Main(string[] args)
         {
-             Console.WriteLine("Digite seu primeiro numero: ");
-            int num1 = Convert.ToInt32(Console.ReadLine());
+            string star = "S";
 
-            expre();
-            string exp = Console.ReadLine();
+            while (star == "S" || star == "s")
+            {
+                Console.WriteLine("Digite seu primeiro numero: ");
+                int num1 = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Digite seu segundo numero: ");
-            int num2 = Convert.ToInt32(Console.ReadLine());
+                expre();
+                string exp = Console.ReadLine();
+
+                Console.WriteLine("Digite seu segundo numero: ");
+                int num2 = Convert.ToInt32(Console.ReadLine());
 
                 if (exp == "/" && num2 == 0)
                 {
-                Console.WriteLine("Não é possivel dividir por 0");
+                    Console.WriteLine("Não é possivel dividir por 0");
                 }
 
                 float result = 0;
 
-                    switch (exp) 
-                    {
-                        case "+":
+                switch (exp)
+                {
+                    case "+":
                         result = num1 + num2;
                         Console.WriteLine($"O Resultado é {result}");
                         break;
-                        case "-":
+                    case "-":
                         result = num1 - num2;
                         Console.WriteLine($"O Resultado é {result}");
                         break;
-                        case "*":
+                    case "*":
                         result = num1 * num2;
                         Console.WriteLine($"O Resultado é {result}");
                         break;
-                        case "/":
-                        result = (float) num1 / num2;
+                    case "/":
+                        result = (float)num1 / num2;
                         Console.WriteLine($"O Resultado é {result}");
                         break;
-                        case "%":
+                    case "%":
                         result = (num1 * num2) / 100;
                         Console.WriteLine($"O Resultado é {result}");
                         break;
-                     }
+                }
+
+                Console.WriteLine("Gostaria de Continuar a conta? (S ou N)");
+                star = Console.ReadLine();
+            }
+            Console.WriteLine("Obg por ter usado a caculadora até a proxima!");
+            Console.ReadLine();
         }
         static void expre()
         {
